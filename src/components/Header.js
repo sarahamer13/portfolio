6dd9829b4084ca4885cf36e8c5f7fe22c2b2062f
Sarah Mahdi <sacred13_heart@yahoo.com>
2023-04-27 19:34:05 -0700

    added name

--- src/components/Header.js
+++ src/components/Header.js
@@ -104,7 +104,7 @@ class Header extends Component {
               <span className="iconify header-icon" data-icon="la:laptop-code" data-inline="false"></span>
               <br/>
               <h1 className="mb-0">
-                {name}
+                Sarah Mahdi
               </h1>
               <div className="title-container">
                 <HeaderTitleTypeAnimation />
