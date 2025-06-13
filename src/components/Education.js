3ccb8d6877677830faa4204645bc7880adc1336d
Sarah Mahdi <sacred13_heart@yahoo.com>
2025-06-13 11:57:34 -0700

    Update Education.js

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,13 +3,18 @@ import { FaGraduationCap } from "react-icons/fa";
 
 class Education extends Component {
   render() {
+    let sectionName, education;
+
     if (this.props.resumeEducation && this.props.resumeBasicInfo) {
-      var sectionName = this.props.resumeBasicInfo.section_name.education;
-      var education = this.props.resumeEducation.map(function (edu, i) {
+      sectionName = this.props.resumeBasicInfo.section_name.education;
+      education = this.props.resumeEducation.map(function (edu, i) {
         return (
           <div className="col-md-6 mb-4" key={i}>
             <div className="card shadow-sm p-4 h-100">
-              <h4 className="mb-2"><FaGraduationCap className="me-2" />{edu.degree}</h4>
+              <h4 className="mb-2">
+                <FaGraduationCap className="me-2" />
+                {edu.degree}
+              </h4>
               <h5 className="text-muted">{edu.school}</h5>
               <p className="text-secondary mb-1"><strong>Years:</strong> {edu.years}</p>
               <ul className="mb-0">
