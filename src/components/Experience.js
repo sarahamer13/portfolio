c314e3064f5221ef291e43bf7abded7311f064cd
Sarah Mahdi <sacred13_heart@yahoo.com>
2025-06-13 11:11:05 -0700

    Update Experience.js

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,59 +4,35 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-import Badge from "react-bootstrap/Badge";
 
 class Experience extends Component {
   render() {
-    if (this.props.resumeExperience && this.props.resumeBasicInfo) {
-      var sectionName = this.props.resumeBasicInfo.section_name.experience;
-      var work = this.props.resumeExperience.map(function (work, i) {
-        const technologies = work.technologies;
-        const mainTechnologies = work.mainTech;
+    let sectionName, work;
 
-        var mainTech = mainTechnologies.map((technology, i) => {
-          return (
-            <Badge pill className="main-badge mr-2 mb-2" key={i}>
-              {technology}
-            </Badge>
-          );
-        });
-        var tech = technologies.map((technology, i) => {
-          return (
-            <Badge pill className="experience-badge mr-2 mb-2" key={i}>
-              {technology}
-            </Badge>
-          );
-        });
+    if (this.props.resumeExperience && this.props.resumeBasicInfo) {
+      sectionName = this.props.resumeBasicInfo.section_name.experience;
+      work = this.props.resumeExperience.map((role, i) => {
         return (
           <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date={work.years}
+            date={role.years}
             iconStyle={{
               background: "#AE944F",
               color: "#fff",
               textAlign: "center",
             }}
-            icon={<i className="fab fa-angular experience-icon"></i>}
+            icon={
+              <i className="fas fa-briefcase experience-icon"></i>
+            }
             key={i}
           >
-            <div style={{ textAlign: "left", marginBottom: "4px" }}>
-              {mainTech}
-            </div>
-
-            <h3
-              className="vertical-timeline-element-title"
-              style={{ textAlign: "left" }}
-            >
-              {work.title}
-            </h3>
-            <h4
-              className="vertical-timeline-element-subtitle"
-              style={{ textAlign: "left" }}
-            >
-              {work.company}
-            </h4>
-            <div style={{ textAlign: "left", marginTop: "15px" }}>{tech}</div>
+            <h3 className="vertical-timeline-element-title">{role.title}</h3>
+            <h4 className="vertical-timeline-element-subtitle">{role.company}</h4>
+            <ul className="mt-3">
+              {role.description.map((point, index) => (
+                <li key={index}>{point}</li>
+              ))}
+            </ul>
           </VerticalTimelineElement>
         );
       });
