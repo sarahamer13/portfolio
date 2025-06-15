3fc5868e60a810f162ea79d83b6e29cd76495d71
Sarah Mahdi <sacred13_heart@yahoo.com>
2025-06-14 22:21:20 -0700

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,23 +2,32 @@ import React, { Component } from "react";
 import Projects from "./Projects";
 import Skills from "./Skills";
 import Experience from "./Experience";
+import Education from "./Education";
 
 class Home extends Component {
   render() {
+    const { resumeData, sharedData } = this.props;
+
     return (
       <div>
         <Projects
-          resumeProjects={this.props.resumeData.projects}
-          resumeBasicInfo={this.props.resumeData.basic_info}
+          resumeProjects={resumeData.projects}
+          resumeBasicInfo={resumeData.basic_info}
         />
         <Skills
-          sharedSkills={this.props.sharedData.skills}
-          resumeBasicInfo={this.props.resumeData.basic_info}
+          sharedSkills={sharedData.skills}
+          resumeBasicInfo={resumeData.basic_info}
         />
         <Experience
-          resumeExperience={this.props.resumeData.experience}
-          resumeBasicInfo={this.props.resumeData.basic_info}
+          resumeExperience={resumeData.experience}
+          resumeBasicInfo={resumeData.basic_info}
         />
+        {resumeData.education && resumeData.basic_info && (
+          <Education
+            resumeEducation={resumeData.education}
+            resumeBasicInfo={resumeData.basic_info}
+          />
+        )}
       </div>
     );
   }
